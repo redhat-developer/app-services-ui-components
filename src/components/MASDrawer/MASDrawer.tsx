555039f76eeb3ac918760c872b80fef5bbc0535b
Donald Labaj <dlabaj@redhat.com>
2021-07-13 11:57:27 -0400

    feat: Added common components too library.
    Added Alerts, Drawer, Loading, Pagination, Table, and Toolbar to the shared library.
    
    Added remaining common components.
    
    Updated package-lock.json
    
    feat: Added common components to library.

---
+++ src/components/MASDrawer/MASDrawer.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  Drawer,
+  DrawerProps,
+  DrawerContent,
+  DrawerPanelContent,
+  DrawerHead,
+  DrawerPanelBody,
+  DrawerActions,
+  DrawerCloseButton,
+  TextContent,
+  Text,
+  TextVariants,
+  Title,
+  TitleSizes,
+  DrawerPanelContentProps,
+  TextProps,
+  TitleProps,
+  DrawerContentBody,
+} from "@patternfly/react-core";
+import { MASLoading } from "@mas/components";
+import "./MASDrawer.css";
+
+export type MASDrawerProps = DrawerProps & {
+  children: React.ReactNode;
+  panelBodyContent?: React.ReactNode;
+  onClose: () => void;
+  drawerData?: any;
+  isLoading?: boolean;
+  drawerPanelContentProps?: Omit<DrawerPanelContentProps, "children">;
+  drawerHeaderProps?: {
+    text?: Omit<TextProps, "children" | "ref"> & {
+      label: string | undefined;
+    };
+    title?: Omit<TitleProps, "children"> & {
+      value: string | undefined;
+    };
+  };
+  ["data-ouia-app-id"]?: string;
+  notRequiredDrawerContentBackground?: boolean | undefined;
+  inlineAlertMessage?: React.ReactNode;
+};
+
+export const MASDrawer: React.FC<MASDrawerProps> = ({
+  onClose,
+  isLoading = false,
+  drawerPanelContentProps,
+  drawerHeaderProps,
+  isExpanded,
+  children,
+  panelBodyContent,
+  onExpand,
+  notRequiredDrawerContentBackground,
+  "data-ouia-app-id": dataOuiaAppId,
+  inlineAlertMessage,
+}: MASDrawerProps) => {
+  const { widths, ...restDrawerPanelContentProps } =
+    drawerPanelContentProps || {};
+  const { text, title } = drawerHeaderProps || {};
+
+  const panelContent = (
+    <DrawerPanelContent
+      widths={widths || { default: "width_50" }}
+      {...restDrawerPanelContentProps}
+    >
+      {isLoading ? (
+        <MASLoading />
+      ) : (
+        <>
+          <DrawerHead>
+            <TextContent>
+              {text?.label && (
+                <Text
+                  component={text?.component || TextVariants.small}
+                  className={text?.className || "kafka-ui-mas-drawer__top-label"}
+                >
+                  {text?.label}
+                </Text>
+              )}
+              {title?.value && (
+                <Title
+                  headingLevel={title?.headingLevel || "h2"}
+                  size={title?.size || TitleSizes["xl"]}
+                  className={title?.className || "kafka-ui-mas-drawer__title"}
+                >
+                  {title?.value}
+                </Title>
+              )}
+            </TextContent>
+            <DrawerActions>
+              <DrawerCloseButton onClick={onClose} />
+            </DrawerActions>
+          </DrawerHead>
+          <DrawerPanelBody>
+            {inlineAlertMessage}
+            {panelBodyContent}
+          </DrawerPanelBody>
+        </>
+      )}
+    </DrawerPanelContent>
+  );
+
+  return (
+    <Drawer
+      isExpanded={isExpanded}
+      onExpand={onExpand}
+      data-ouia-app-id={dataOuiaAppId}
+    >
+      <DrawerContent
+        panelContent={panelContent}
+        className={
+          notRequiredDrawerContentBackground ? "pf-m-no-background" : ""
+        }
+      >
+        <DrawerContentBody className="kafka-ui-mas-drawer__drawer-content-body">
+          {children}
+        </DrawerContentBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './MASAlerts';
+export * from './MASDrawer';
+export * from './MASLoading';
+export * from './MASPagination';
+export * from './MASTable';
+export * from './MASToolbar';
