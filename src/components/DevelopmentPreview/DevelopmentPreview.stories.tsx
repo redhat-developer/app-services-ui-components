115ff7713123dc756f99816f1e365bf72bec8c83
hemahg <hhg@redhat.com>
2021-12-16 12:45:19 +0530

    Incorporating Review comment

--- src/components/DevelopmentPreview/DevelopmentPreview.stories.tsx
+++ src/components/DevelopmentPreview/DevelopmentPreview.stories.tsx
@@ -1,5 +1,6 @@
+import { PageSection } from "@patternfly/react-core";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { DevelopmentPreview, DevelopmentPreviewProps } from './DevelopmentPreview';
+import { DevelopmentPreview } from './DevelopmentPreview';
 import DevelopmentPreviewI18N from "./DevelopmentPreview-i18n.json";
 
 export default {
@@ -19,4 +20,22 @@ const Template: ComponentStory<typeof DevelopmentPreview> = (args) => (
 );
 
 export const showDevPreview = Template.bind({});
-showDevPreview.args = {};
+showDevPreview.args = {
+    children:
+        <PageSection>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a
+                type specimen book. It has survived not only five centuries,
+                but also the leap into electronic typesetting,
+                remaining essentially unchanged.
+
+                It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently
+                with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.
+            </p>
+        </PageSection>
+};
