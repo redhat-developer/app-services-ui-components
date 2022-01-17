9ee7c63c4ae152e9246bee4b610fdac245d54395
Cliodhna Fullam <cfullam@redhat.com>
2022-01-17 11:54:46 +0000

    adding a 'ready' plain inline alert

---
+++ src/PoCs/readyIdea.stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { KafkaStatusPopover, KafkaStatusPopoverBody, } from './readyIdea';
+
+export default {
+  title: 'NewAlert/KafkaStatusPopover',
+  component: KafkaStatusPopover,
+  subcomponents: { KafkaStatusPopoverBody },
+  args: {
+    children: <a>Creating</a>,
+    shouldStartOpen: true,
+  }, 
+} as ComponentMeta<typeof KafkaStatusPopover>;
+
+const Template: ComponentStory<typeof KafkaStatusPopover> = (args) => (
+    <div style={{ paddingTop: 280, height: 600 }}>
+        <KafkaStatusPopover {...args} />
+    </div>
+);
+
+export const Pending = Template.bind({});
+Pending.args = {
+  currentState: "pending",
+};
+
+export const Provisioning = Template.bind({});
+Provisioning.args = {
+  currentState: "provisioning",
+};
+
+export const Preparing = Template.bind({});
+Preparing.args = {
+  currentState: "preparing",
+};
+
+
