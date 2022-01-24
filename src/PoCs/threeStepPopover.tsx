3aa34a3e2e76b9641abef925de90da0d2839bdef
Cliodhna Fullam <cfullam@redhat.com>
2022-01-24 10:28:40 +0000

    adding docs section for the different steps

--- src/PoCs/threeStepPopover.tsx
+++ src/PoCs/threeStepPopover.tsx
@@ -9,6 +9,19 @@ type KafkaStatusPopoverProps = {
   shouldStartOpen?: boolean;
 } & KafkaStatusPopoverBodyProps;
 
+/**
+*<strong> Introduction/Background </strong>
+*
+*This is a popover that can be reached from the Kafka Instance table while the status of the instance in question is “Creating”. A Kafka instance usually takes a few minutes to create. The purpose of this popover is to give the user some insights into the creation process of their instance while they wait for it to be available to use. 
+*
+*   -The text at the top explains that the instance will be ready shortly & suggests that the user creates  a service account while they wait. (Provides a link to the “Connections tab where this is done).
+*
+*   -The steps shown in the progress stepper are statuses that show in the api. They have been grouped together under the one overall “Creating status” in the table.There is some descriptive text accompanying each step too. When these steps are complete the instance will be ready for use
+*
+*   -The popover is only available while instance creation is in progress.
+*
+*   -After creation is complete the table displays the ready status and the popover is no longer available.
+*/
 export const KafkaStatusPopover: FunctionComponent<KafkaStatusPopoverProps> = ({
   shouldStartOpen = false,
   currentState,
--- src/PoCs/threeSteppopover.stories.tsx
+++ src/PoCs/threeSteppopover.stories.tsx
@@ -23,15 +23,37 @@ export const Pending = Template.bind({});
 Pending.args = {
   currentState: "pending",
 };
+Pending.parameters = {
+  docs: {
+    description: {
+      story: `
+      A user has just selected to create a Kafka Instance. The creation of their instance is pending.
+      `,
+    },
+  },
+};
 
 export const Provisioning = Template.bind({});
 Provisioning.args = {
   currentState: "provisioning",
+};
+Provisioning.parameters = {
+  docs: {
+    description: {
+        story: 'The progress stepper is on the second step in the creation process. “Pending” is complete and “Provisioning” is in progress. Creation of the instance has begun.'
+    },
+  },
 };
 
 export const Preparing = Template.bind({});
 Preparing.args = {
   currentState: "preparing",
 };
-
+Preparing.parameters = {
+  docs: {
+    description: {
+      story: 'Provisioning” is complete and the ”Preparing” step of the instance is in progress. This is the final step in the creation process. Once this step is complete the instance can be used. The popover will no longer be available.',
+    },
+  },
+};
 
