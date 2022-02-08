e39b322ae635590aa375808fa398c6af4d35bc87
suyash-naithani <suyash.naithani@gmail.com>
2022-02-08 20:54:04 +0530

    Add stories for details tab of instance drawer

---
+++ src/KafkaInstanceDrawer/DetailsTab.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { subHours } from 'date-fns'
+import React from 'react'
+
+import { DetailsTab } from './DetailsTab'
+
+export default {
+  title: 'Components/KafkaInstanceDrawer/DetailsTab',
+  component: DetailsTab,
+  args: {
+    id:'c7tkgchgb2d04561srg',
+    owner:'snaithan_kafka_devexp',
+    region:'US East, N. Virginia',
+    created_at:new Date(),
+    updated_at:new Date()
+  },
+} as ComponentMeta<typeof DetailsTab>
+
+const Template: ComponentStory<typeof DetailsTab> = (args) => (
+  <DetailsTab {...args} />
+)
+
+export const JustCreated = Template.bind({})
+JustCreated.args = {}
+
+export const CreatedSomeTimeAgo = Template.bind({})
+CreatedSomeTimeAgo.args = {
+  created_at:subHours(new Date(),25)
+}
+
+export const CreatedLongTimeAgo = Template.bind({})
+CreatedLongTimeAgo.args = {
+  created_at:subHours(new Date(),46)
+}
+
+
+
+
+
---
+++ src/KafkaInstanceDrawer/DetailsTabAlert.tsx
@@ -0,0 +1,38 @@
+import { Alert } from '@patternfly/react-core'
+import { addHours, differenceInHours } from 'date-fns'
+import React from 'react'
+import { Trans } from 'react-i18next'
+import { FormatDate } from '../shared'
+
+type DetailsTabAlertProps = {
+  creationDate: Date
+}
+
+export const DetailsTabAlert: React.FunctionComponent<DetailsTabAlertProps> = ({
+  creationDate,
+}) => {
+  const expiryDate = addHours(creationDate, 48)
+  const hoursLeft = differenceInHours(expiryDate,Date.now())
+  return (
+    <Alert
+      variant={
+        hoursLeft > 24
+          ? 'info'
+          : hoursLeft < 24 && hoursLeft > 5
+          ? 'warning'
+          : 'danger'
+      }
+      title={
+        <Trans
+          i18nKey="common:will_expire"
+          components={{
+            time: <FormatDate date={expiryDate} format="expiration" />,
+          }}
+        />
+      }
+      aria-live="polite"
+      isInline
+      className="pf-u-mb-lg"
+    />
+  )
+}
