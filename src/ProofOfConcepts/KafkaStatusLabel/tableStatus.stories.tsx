31b2269f119a19cbf763989ab436ae204d3d3071
cfullam1 <89915947+cfullam1@users.noreply.github.com>
2022-02-11 16:42:03 +0000

    chore:  Status Section for Kafka Table  (#132)
    * basic-- getting components and text together in the different stories
    
    * adding icon and text colours
    
    * adding flex layout
    
    * flexitems
    
    * resizing icons and adjusting gutter size
    
    * adding a docs section
    
    * adding a preliminary failed status
    
    * fixing ready icon position
    
    * fixing ready icon position
    
    * changing the naming scheme and the names for the css classes
    
    * updating creating user case name
    
    * reordered stories and made two separate classes for icons
    
    * reordered the stories
    
    * new files
    
    * moving all of the story version filed to the one tableStatus file
    
    * added arg back
    
    * adding ready as default story
    
    * reorganize code
    
    * Add an example default story that shows all statuses at the same time
    
    * Update tableStatus.stories.tsx
    
    * Adding documentation
    
    * adding docs for the first story (with prettier)
    
    * Adding text to docs (and running prettier)
    
    Co-authored-by: Riccardo Forina <rforina@redhat.com>

---
+++ src/ProofOfConcepts/KafkaStatusLabel/tableStatus.stories.tsx
@@ -0,0 +1,108 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import { TableStatus } from "./tableStatus";
+
+export default {
+  component: TableStatus,
+  args: {
+    value: "ready",
+  },
+} as ComponentMeta<typeof TableStatus>;
+
+const Template: ComponentStory<typeof TableStatus> = (args) => (
+  <TableStatus {...args} />
+);
+
+export const AllStatuesAtOnce = () => (
+  <>
+    <TableStatus value={"ready"} />
+    <br />
+    <TableStatus value={"creating"} />
+    <br />
+    <TableStatus value={"creatingWarning"} />
+    <br />
+    <TableStatus value={"creatingError"} />
+    <br />
+    <TableStatus value={"deleting"} />
+    <br />
+    <TableStatus value={"failed"} />
+  </>
+);
+
+export const Ready = Template.bind({});
+Ready.args = {
+  value: "ready",
+};
+Ready.parameters = {
+  docs: {
+    description: {
+      story:
+        "The Kafka instance was successfully created and is now either ready for use or in use.",
+    },
+  },
+};
+
+export const Creating = Template.bind({});
+Creating.args = {
+  value: "creating",
+};
+Creating.parameters = {
+  docs: {
+    description: {
+      story:
+        'A user has just selected to create a Kafka instance. The "Creating" status is shown in the table along with some grey text informing the user that their instance will be ready shortly. (The "Creating" status is clickable which opens a popover with more in-depth information about the creation process).',
+    },
+  },
+};
+
+export const CreatingOver15Minutes = Template.bind({});
+CreatingOver15Minutes.args = {
+  value: "creatingWarning",
+};
+CreatingOver15Minutes.parameters = {
+  docs: {
+    description: {
+      story:
+        'The instance creation is taking longer than fifteen minutes. This is significantly longer than expected. A plain inline warning alert displays below the "Creating" status in the table.',
+    },
+  },
+};
+
+export const CreatingOver30Minutes = Template.bind({});
+CreatingOver30Minutes.args = {
+  value: "creatingError",
+};
+CreatingOver30Minutes.parameters = {
+  docs: {
+    description: {
+      story:
+        'The instance creation is taking longer than thirty minutes. This is significantly longer than expected. An error plain inline alert displays below the "Creating" status in the table.',
+    },
+  },
+};
+
+export const Deleting = Template.bind({});
+Deleting.args = {
+  value: "deleting",
+};
+Deleting.parameters = {
+  docs: {
+    description: {
+      story:
+        "The user has selected to delete the Kafka instance and it is in the process of being deleted.",
+    },
+  },
+};
+
+export const Failed = Template.bind({});
+Failed.args = {
+  value: "failed",
+};
+Failed.parameters = {
+  docs: {
+    description: {
+      story: "Something has gone wrong with the Kafka instance .",
+    },
+  },
+};
