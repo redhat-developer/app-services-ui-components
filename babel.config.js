526e6fb67b68b8d248cba5b008b6e72819a659f5
Riccardo Forina <rforina@redhat.com>
2022-02-12 10:00:04 +0100

    chore: use the new automatic jsx transform

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     "@babel/preset-typescript",
-    "@babel/preset-react",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
   ],
 };
