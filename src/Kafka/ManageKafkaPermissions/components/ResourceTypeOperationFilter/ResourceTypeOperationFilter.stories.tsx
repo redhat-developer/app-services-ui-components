d7a280b47619c1ebc06ef6782bfc6e43f4a81d90
Ajay Pratap <apratap@redhat.com>
2022-02-15 13:58:30 +0530

    add few more stories

--- src/Kafka/ManageKafkaPermissions/components/ResourceTypeOperationFilter/ResourceTypeOperationFilter.stories.tsx
+++ src/Kafka/ManageKafkaPermissions/components/ResourceTypeOperationFilter/ResourceTypeOperationFilter.stories.tsx
@@ -2,13 +2,124 @@ import React, { useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { ResourceTypeOperationFilter } from "./ResourceTypeOperationFilter";
+import { AclResourceType, AclOperation } from "../../types";
 
 export default {
   component: ResourceTypeOperationFilter,
   args: {},
 } as ComponentMeta<typeof ResourceTypeOperationFilter>;
 
-const Template: ComponentStory<typeof ResourceTypeOperationFilter> = () => {
+const Template: ComponentStory<typeof ResourceTypeOperationFilter> = (args) => {
+  return <ResourceTypeOperationFilter {...args} />;
+};
+
+export const NoItemChecked = Template.bind({});
+
+export const SomeItemChecked = Template.bind({});
+SomeItemChecked.args = {
+  checkedItems: [
+    {
+      id: `${AclResourceType.Group}-${AclOperation.All}`,
+      name: AclOperation.All,
+    },
+    {
+      id: `${AclResourceType.Group}-${AclOperation.Read}`,
+      name: AclOperation.Read,
+    },
+    {
+      id: `${AclResourceType.Group}-${AclOperation.Delete}`,
+      name: AclOperation.Delete,
+    },
+    {
+      id: `${AclResourceType.Group}-${AclOperation.Describe}`,
+      name: AclOperation.Describe,
+    },
+    {
+      id: `${AclResourceType.Cluster}-${AclOperation.Alter}`,
+      name: AclOperation.Alter,
+    },
+  ],
+};
+
+export const AllItemsChecked = Template.bind({});
+AllItemsChecked.args = {
+  checkedItems: [
+    {
+      name: AclOperation.All,
+      id: `${AclResourceType.Group}-${AclOperation.All}`,
+    },
+    {
+      name: AclOperation.Read,
+      id: `${AclResourceType.Group}-${AclOperation.Read}`,
+    },
+    {
+      name: AclOperation.Delete,
+      id: `${AclResourceType.Group}-${AclOperation.Delete}`,
+    },
+    {
+      name: AclOperation.Describe,
+      id: `${AclResourceType.Group}-${AclOperation.Describe}`,
+    },
+    {
+      name: AclOperation.Alter,
+      id: `${AclResourceType.Cluster}-${AclOperation.Alter}`,
+    },
+    {
+      name: AclOperation.Describe,
+      id: `${AclResourceType.Cluster}-${AclOperation.Describe}`,
+    },
+    {
+      name: AclOperation.All,
+      id: `${AclResourceType.Topic}-${AclOperation.All}`,
+    },
+    {
+      name: AclOperation.Alter,
+      id: `${AclResourceType.Topic}-${AclOperation.Alter}`,
+    },
+    {
+      name: AclOperation.AlterConfigs,
+      id: `${AclResourceType.Topic}-${AclOperation.AlterConfigs}`,
+    },
+    {
+      name: AclOperation.Create,
+      id: `${AclResourceType.Topic}-${AclOperation.Create}`,
+    },
+    {
+      name: AclOperation.Delete,
+      id: `${AclResourceType.Topic}-${AclOperation.Delete}`,
+    },
+    {
+      name: AclOperation.Describe,
+      id: `${AclResourceType.Topic}-${AclOperation.Describe}`,
+    },
+    {
+      name: AclOperation.DescribeConfigs,
+      id: `${AclResourceType.Topic}-${AclOperation.DescribeConfigs}`,
+    },
+    {
+      name: AclOperation.Read,
+      id: `${AclResourceType.Topic}-${AclOperation.Read}`,
+    },
+    {
+      name: AclOperation.Write,
+      id: `${AclResourceType.Topic}-${AclOperation.Write}`,
+    },
+    {
+      name: AclOperation.All,
+      id: `${AclResourceType.TransactionalId}-${AclOperation.All}`,
+    },
+    {
+      name: AclOperation.Alter,
+      id: `${AclResourceType.TransactionalId}-${AclOperation.Alter}`,
+    },
+    {
+      name: AclOperation.Describe,
+      id: `${AclResourceType.TransactionalId}-${AclOperation.Describe}`,
+    },
+  ],
+};
+
+export const InteractiveExample = () => {
   const [checkedItems, setCheckedItems] = useState([]);
   return (
     <ResourceTypeOperationFilter
@@ -17,6 +128,3 @@ const Template: ComponentStory<typeof ResourceTypeOperationFilter> = () => {
     />
   );
 };
-
-export const ResourceTypeOperationFilterStory = Template.bind({});
-ResourceTypeOperationFilterStory.storyName = "ResourceTypeOperationFilter";
--- src/Kafka/ManageKafkaPermissions/components/ResourceTypeOperationFilter/ResourceTypeOperationFilter.test.tsx
+++ src/Kafka/ManageKafkaPermissions/components/ResourceTypeOperationFilter/ResourceTypeOperationFilter.test.tsx
@@ -5,41 +5,19 @@ import { render, waitForI18n, waitForPopper } from "../../../../test-utils";
 import * as stories from "./ResourceTypeOperationFilter.stories";
 import { userEvent } from "@storybook/testing-library";
 
-const { ResourceTypeOperationFilterStory } = composeStories(stories);
-
-// jest.mock("popper.js", () => {
-//   const PopperJS = jest.requireActual("popper.js");
-
-//   return class {
-//     static placements = PopperJS.placements;
-
-//     constructor() {
-//       return {
-//         destroy: () => {},
-//         scheduleUpdate: () => {},
-//       };
-//     }
-//   };
-// });
+const { NoItemChecked } = composeStories(stories);
 
 describe("<ResourceTypeOperationFilter/>", () => {
   it("should render acls treeview and expend", async () => {
     const onCheckedItemsChange = jest.fn();
     const comp = render(
-      <ResourceTypeOperationFilterStory
+      <NoItemChecked
         checkedItems={[]}
         onCheckedItemsChange={onCheckedItemsChange}
       />
     );
 
     await waitForI18n(comp);
-
-    // const menuToggle = within(
-    //   await comp.findByTestId("acls-treeview-menu-toggle")
-    // );
-
-    // const button = await comp.getByText(/Filter by resource type operation/i);
-    // button.click();
 
     userEvent.click(await comp.findByText("Filter by resource type operation"));
     await waitForPopper();
