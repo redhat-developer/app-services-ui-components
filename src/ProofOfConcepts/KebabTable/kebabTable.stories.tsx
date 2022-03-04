a80a519777391d17aaa71f9f558eeb70df3a6e57
Cliodhna Fullam <cfullam@redhat.com>
2022-03-04 16:49:01 +0000

    running prettier

--- src/ProofOfConcepts/KebabTable/kebabTable.stories.tsx
+++ src/ProofOfConcepts/KebabTable/kebabTable.stories.tsx
@@ -1,14 +1,12 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import React from 'react';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
 
-import { KebabTable } from './kebabTable';
+import { KebabTable } from "./kebabTable";
 
 export default {
-  title: 'Proof Of Concepts/KebabTable',
+  title: "Proof Of Concepts/KebabTable",
   component: KebabTable,
-  args: {
-
-  },
+  args: {},
 } as ComponentMeta<typeof KebabTable>;
 
 const Template: ComponentStory<typeof KebabTable> = (args) => (
--- src/ProofOfConcepts/KebabTable/kebabTable.tsx
+++ src/ProofOfConcepts/KebabTable/kebabTable.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React from "react";
 import {
   TableComposable,
   Thead,
@@ -8,88 +8,103 @@ import {
   Tbody,
   Td,
   ActionsColumn,
-  IAction
-} from '@patternfly/react-table';
+  IAction,
+} from "@patternfly/react-table";
 
 interface Repository {
   name: string;
-  cloudProvider:string;
-  region:string;
-  owner:string;
-  status:string;
-  timeCreated:string;
+  cloudProvider: string;
+  region: string;
+  owner: string;
+  status: string;
+  timeCreated: string;
 }
 export const KebabTable: React.FunctionComponent = () => {
   // In real usage, this data would come from some external source like an API via props.
   const repositories: Repository[] = [
-    { name: 'instanceName', cloudProvider:'Amazon Web Services', region:'US East, N. Virginia ', owner:'owner@redhat.com',  status:'Ready', timeCreated:'1 day ago'},
-    { name: 'instanceName2', cloudProvider:'Amazon Web Services', region:'US East, N. Virginia ', owner:'owner2@redhat.com',  status:'Ready', timeCreated:'1 day ago'}
+    {
+      name: "instanceName",
+      cloudProvider: "Amazon Web Services",
+      region: "US East, N. Virginia ",
+      owner: "owner@redhat.com",
+      status: "Ready",
+      timeCreated: "1 day ago",
+    },
+    {
+      name: "instanceName2",
+      cloudProvider: "Amazon Web Services",
+      region: "US East, N. Virginia ",
+      owner: "owner2@redhat.com",
+      status: "Ready",
+      timeCreated: "1 day ago",
+    },
   ];
   const columnNames = {
-    name: 'Name',
-    cloudProvider:'Cloud provider',
-    region:'Region',
-    owner:'Owner',
-    status:'Status',
-    timeCreated:'Time created'
+    name: "Name",
+    cloudProvider: "Cloud provider",
+    region: "Region",
+    owner: "Owner",
+    status: "Status",
+    timeCreated: "Time created",
   };
 
   const defaultActions = (repo: Repository): IAction[] => [
     {
-      title: 'View details',
-      onClick: () => console.log(`clicked on View details, on row ${repo.name}`)
+      title: "View details",
+      onClick: () =>
+        console.log(`clicked on View details, on row ${repo.name}`),
     },
     {
-      title: 'View connections information',
-      onClick: () => console.log(`clicked on View connections information, on row ${repo.name}`)
+      title: "View connections information",
+      onClick: () =>
+        console.log(
+          `clicked on View connections information, on row ${repo.name}`
+        ),
     },
     {
-      isSeparator: true
+      isSeparator: true,
     },
     {
-      title: 'Delete instance',
-      onClick: () => console.log(`clicked on Delete instance, on row ${repo.name}`)
-    }
+      title: "Delete instance",
+      onClick: () =>
+        console.log(`clicked on Delete instance, on row ${repo.name}`),
+    },
   ];
   return (
-    
-      <TableComposable aria-label="Actions table">
-        <Thead>
-          <Tr>
-            <Th>{columnNames.name}</Th>
-            <Th>{columnNames.cloudProvider}</Th>
-            <Th>{columnNames.region}</Th>
-            <Th>{columnNames.owner}</Th>
-            <Th>{columnNames.status}</Th>
-            <Th>{columnNames.timeCreated}</Th>
-            <Td></Td>
-          </Tr>
-        </Thead>
-        <Tbody>
-        {repositories.map(repo => {
-            // Arbitrary logic to determine which rows get which actions in this example
-            let rowActions: IAction[] | null = defaultActions(repo);
+    <TableComposable aria-label="Actions table">
+      <Thead>
+        <Tr>
+          <Th>{columnNames.name}</Th>
+          <Th>{columnNames.cloudProvider}</Th>
+          <Th>{columnNames.region}</Th>
+          <Th>{columnNames.owner}</Th>
+          <Th>{columnNames.status}</Th>
+          <Th>{columnNames.timeCreated}</Th>
+          <Td></Td>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {repositories.map((repo) => {
+          // Arbitrary logic to determine which rows get which actions in this example
+          let rowActions: IAction[] | null = defaultActions(repo);
 
-
-            return (
-              <Tr key={repo.name}>
-                <Td dataLabel={columnNames.name}>{repo.name}</Td>
-                <Td dataLabel={columnNames.cloudProvider}>{repo.cloudProvider}</Td>
-                <Td dataLabel={columnNames.region}>{repo.region}</Td>
-                <Td dataLabel={columnNames.owner}>{repo.owner}</Td>
-                <Td dataLabel={columnNames.status}>{repo.status}</Td>
-                <Td dataLabel={columnNames.timeCreated}>{repo.timeCreated}</Td>
-                <Td isActionCell>
-                  {rowActions ? (
-                    <ActionsColumn
-                      items={rowActions} 
-                    />
-                  ) : null}
-                </Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </TableComposable>
+          return (
+            <Tr key={repo.name}>
+              <Td dataLabel={columnNames.name}>{repo.name}</Td>
+              <Td dataLabel={columnNames.cloudProvider}>
+                {repo.cloudProvider}
+              </Td>
+              <Td dataLabel={columnNames.region}>{repo.region}</Td>
+              <Td dataLabel={columnNames.owner}>{repo.owner}</Td>
+              <Td dataLabel={columnNames.status}>{repo.status}</Td>
+              <Td dataLabel={columnNames.timeCreated}>{repo.timeCreated}</Td>
+              <Td isActionCell>
+                {rowActions ? <ActionsColumn items={rowActions} /> : null}
+              </Td>
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </TableComposable>
   );
 };
