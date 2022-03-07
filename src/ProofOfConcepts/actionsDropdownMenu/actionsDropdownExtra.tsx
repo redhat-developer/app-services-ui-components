f6d398666b4c9a9a56512a1333f5062bdb74a91b
Cliodhna Fullam <cfullam@redhat.com>
2022-03-07 14:47:35 +0000

    adding some story options for now to dechide which text is favoured

---
+++ src/ProofOfConcepts/actionsDropdownMenu/actionsDropdownExtra.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownGroup,
+  DropdownItem,
+  DropdownSeparator,
+} from "@patternfly/react-core";
+
+export class ActionsDropdownExtra extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.onToggle = (isOpen) => {
+      this.setState({
+        isOpen,
+      });
+    };
+    this.onSelect = (event) => {
+      this.setState({
+        isOpen: !this.state.isOpen,
+      });
+      this.onFocus();
+    };
+    this.onFocus = () => {
+      const element = document.getElementById("toggle-id-3");
+      element.focus();
+    };
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    const dropdownItems = [
+      <DropdownGroup label="View information" key="View information">
+        <DropdownItem key="View information:Details">View details</DropdownItem>
+        <DropdownItem key="View information:Connection">
+          {" "}
+          View connection information{" "}
+        </DropdownItem>
+        <DropdownSeparator key="dropdown separator" />
+      </DropdownGroup>,
+      <DropdownGroup key="Actions">
+        <DropdownItem key="Actions:Delete">Delete instance</DropdownItem>
+      </DropdownGroup>,
+    ];
+    return (
+      <Dropdown
+        onSelect={this.onSelect}
+        toggle={
+          <DropdownToggle onToggle={this.onToggle} id="toggle-id-3">
+            Actions
+          </DropdownToggle>
+        }
+        isOpen={isOpen}
+        dropdownItems={dropdownItems}
+        isGrouped
+      />
+    );
+  }
+}
