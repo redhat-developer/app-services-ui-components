bee8e83b92fc159b0177eede4ac06b450cc36522
Cliodhna Fullam <cfullam@redhat.com>
2022-03-11 16:13:06 +0000

    Making the table rows clickable : updating the text to match figma design doc: changing the setup of the table (from being hardcoded now have a mockrepo)

--- src/ProofOfConcepts/MessageTable/messageTable.tsx
+++ src/ProofOfConcepts/MessageTable/messageTable.tsx
@@ -1,4 +1,4 @@
-import React, { VoidFunctionComponent } from "react";
+import React from "react";
 import {
   TableComposable,
   Thead,
@@ -9,15 +9,15 @@ import {
 } from "@patternfly/react-table";
 
 interface Repository {
-  partition: string;
-  offset: string | null;
-  timestamp: string | null;
-  key: string;
-  value: string;
-  headers: string;
+  partition: string | object;
+  offset: string | null | object;
+  timestamp: string | null | object;
+  key: string | null | object;
+  value: string | object;
+  header: string | object;
 }
 
-export const Table: VoidFunctionComponent = () => {
+export const Table: React.FunctionComponent = () => {
   // In real usage, this data would come from some external source like an API via props.
   const repositories: Repository[] = [
     {
@@ -25,40 +25,134 @@ export const Table: VoidFunctionComponent = () => {
       offset: "9",
       timestamp: "Feb 6, 2022, 11:26:00:456 AM",
       key: "this-is-a-sample-key",
-      value:
-        '{"menu":{ "id":{"20", "value":"File", "popup":{ "menuitem":[ {"value":"New", "onclick":"CreateNewDoc()"}, {"value":"...show more',
-      headers:
-        '{"key": Strut{order_id=3}", "payload":{"order_id":{ "string":"3" }, "customer_id":{"string":"695" }, "order_total_en... ',
+      value: (
+        <div>
+          {
+            " {”menu”:{ ”id”:{“20”, ”value”:”File”, ”popup”:{ ”menuitem”:[ {”value”:”New”, ”onclick”:”CreateNewDoc()”}, {”value”:” . . . "
+          }
+          <a>Show more</a>
+        </div>
+      ),
+      header: (
+        <div>
+          {
+            '{”key”: Struct{order_id=3}”, ”payload”:{“order_id”:{ "string”:”3” }, ”customer_id”:{“string”:”695” }, ”order_total_en . . . '
+          }
+          <a>Show more</a>
+        </div>
+      ),
     },
+
     {
       partition: "0",
       offset: "8",
-      timestamp: "Feb 6, 2022, 11:26:00:456 AM",
-      key: "this-is-a-sample-key",
-      value:
-        '{"menu":{ "id":{"20", "value":"File", "popup":{ "menuitem":[ {"value":"New", "onclick":"CreateNewDoc()"}, {"value":"...show more',
-      headers:
-        '{"key": Strut{order_id=3}", "payload":{"order_id":{ "string":"3" }, "customer_id":{"string":"695" }, "order_total_en... ',
+      timestamp: "Feb 6, 2022, 11:25:55:999 AM",
+      key: "this-is-another-sample-key",
+      value: (
+        <div>
+          {
+            "{”menu”:{ ”id”:{“19”, ”value”:”File”, ”popup”:{ ”menuitem”:[ {”value”:”New”, ”onclick”:”CreateNewDoc()”}, {”value”:” . . .  "
+          }
+          <a>Show more</a>
+        </div>
+      ),
+      header: '{”trace-id”: "b1cf7a545dg6sa8d7ac:dc7a9fg651ad767j83:1”}',
     },
+
     {
       partition: "0",
       offset: "7",
-      timestamp: "Feb 6, 2022, 11:26:00:456 AM",
-      key: "this-is-a-sample-key",
+      timestamp: "Feb 6, 2022, 11:24:53:123 AM",
+      key: "null",
+      value: "Test",
+      header: "[]",
+    },
+
+    {
+      partition: "0",
+      offset: "6",
+      timestamp: "Feb 6, 2022, 11:20:00:321 AM",
+      key: "15",
       value:
-        '{"menu":{ "id":{"20", "value":"File", "popup":{ "menuitem":[ {"value":"New", "onclick":"CreateNewDoc()"}, {"value":"...show more',
-      headers:
-        '{"key": Strut{order_id=3}", "payload":{"order_id":{ "string":"3" }, "customer_id":{"string":"695" }, "order_total_en... ',
+        "{”menu”:{ ”id”:{“19”, ”popup”:{ ”menuitem”:[ {”value”:”New”, ”onclick”:”CreateNewDoc()”} ] } }}",
+      header: "[]",
+    },
+
+    {
+      partition: "0",
+      offset: "5",
+      timestamp: "Feb 6, 2022, 11:18:10:001 AM",
+      key: "15",
+      value: '{"test":{ "id":"10"}}',
+      header: "{”test-id”: 11”}",
+    },
+
+    {
+      partition: "0",
+      offset: "4",
+      timestamp: "Feb 6, 2022, 11:16:00:007 AM",
+      key: "this-is-a-long-sample-key-something-something",
+      value: '{"test":{ "id":"10"}}',
+      header: (
+        <div>
+          {
+            '{"menu":{ "id":"19", "value":"File", "popup":{"menuitem":[ {"value":"New", "onclick": "CreateNewDoc()"}, {"value": . . .'
+          }{" "}
+          <a>Show more</a>
+        </div>
+      ),
+    },
+
+    {
+      partition: "0",
+      offset: "3",
+      timestamp: "Feb 6, 2022, 11:15:50:978 AM",
+      key: "15",
+      value: '{"test":{ "id":"9"}}',
+      header: "{”test-id”: 110”}",
+    },
+
+    {
+      partition: "0",
+      offset: "2",
+      timestamp: "Feb 6, 2022, 11:15:01:567 AM",
+      key: (
+        <div>
+          {
+            "this-is-a-super-long-sample-key-something-something-something-else-on..."
+          }
+          <a>Show more</a>
+        </div>
+      ),
+      value: '{"test":{ "id":"10"}}',
+      header: "[]",
+    },
+
+    {
+      partition: "0",
+      offset: "1",
+      timestamp: "Feb 6, 2022, 11:00:00:777 AM",
+      key: "12",
+      value: '{"test":{ "id":"9}}',
+      header: "{”test-id”: 110”}",
+    },
+    {
+      partition: "0",
+      offset: "0",
+      timestamp: "Feb 6, 2022, 10:55:08:133 AM",
+      key: "null",
+      value: " Hello world",
+      header: "[]",
     },
   ];
 
   const columnNames = {
     partition: "Partition",
     offset: "Offset",
-    timestamp: "Time Stamp",
+    timestamp: "Timestamp",
     key: "Key",
     value: "Value",
-    headers: "Headers",
+    header: "Header",
   };
 
   // In this example, selected rows are tracked by the repo names from each row. This could be any unique identifier.
@@ -87,7 +181,7 @@ export const Table: VoidFunctionComponent = () => {
           <Th>{columnNames.timestamp}</Th>
           <Th>{columnNames.key}</Th>
           <Th>{columnNames.value}</Th>
-          <Th>{columnNames.headers}</Th>
+          <Th>{columnNames.header}</Th>
         </Tr>
       </Thead>
       <Tbody>
@@ -103,7 +197,7 @@ export const Table: VoidFunctionComponent = () => {
             <Td dataLabel={columnNames.timestamp}>{repo.timestamp}</Td>
             <Td dataLabel={columnNames.key}>{repo.key}</Td>
             <Td dataLabel={columnNames.value}>{repo.value}</Td>
-            <Td dataLabel={columnNames.headers}>{repo.headers}</Td>
+            <Td dataLabel={columnNames.header}>{repo.header}</Td>
           </Tr>
         ))}
       </Tbody>
