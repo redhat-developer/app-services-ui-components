f6a16796b5eb05c2e9dd6e38eccdf07405d76367
suyash-naithani <66817941+suyash-naithani@users.noreply.github.com>
2022-03-23 16:03:14 +0530

    chore: Add select for resource permission select (#231)

---
+++ src/Kafka/ManageKafkaPermissions/components/ResourcePermission.test.tsx
@@ -0,0 +1,35 @@
+import { composeStories } from "@storybook/testing-react";
+import * as stories from "./ResourcePermission.stories";
+import { userEvent } from "@storybook/testing-library";
+import { render, waitForI18n } from "../../../test-utils";
+
+const { WorksWithModal, InitialState } = composeStories(stories);
+
+describe("Resource type", () => {
+  it("should render a select component for resource permission", async () => {
+    const onChangeValue = jest.fn();
+    const comp = render(<WorksWithModal onChangeValue={onChangeValue} />);
+    await waitForI18n(comp);
+    const permissionValue = await comp.getAllByText("Allow");
+    expect(permissionValue[0]).toBeInTheDocument();
+    expect(permissionValue[1]).toBeInTheDocument();
+    expect(await comp.findByText("Deny")).toBeInTheDocument();
+    userEvent.click(await comp.findByText("Deny"));
+    expect(await comp.findByText("Allow")).toBeInTheDocument();
+    expect(await comp.queryByText("Deny")).not.toBeInTheDocument();
+  });
+  it("should show a select component for resource type with validation error ", async () => {
+    const onChangeValue = jest.fn();
+    const comp = render(<InitialState onChangeValue={onChangeValue} />);
+    await waitForI18n(comp);
+    expect(await comp.findByText("Allow")).toBeInTheDocument();
+    userEvent.click(await comp.findByText("Allow"));
+    expect(await comp.findByText("Deny")).toBeInTheDocument();
+    const permissionValue = await comp.getAllByText("Allow");
+    expect(permissionValue[0]).toBeInTheDocument();
+    expect(permissionValue[1]).toBeInTheDocument();
+    userEvent.click(await comp.findByText("Deny"));
+    expect(await comp.findByText("Allow")).toBeInTheDocument();
+    expect(await comp.queryByText("Deny")).not.toBeInTheDocument();
+  });
+});
