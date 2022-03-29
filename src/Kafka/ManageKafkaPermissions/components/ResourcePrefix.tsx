6dd12eb1b2f35b9b4233e3e3665306b46d5865aa
suyash-naithani <suyash.naithani@gmail.com>
2022-03-29 18:07:51 +0530

    Add a typeahead select for resource prefix

--- src/Kafka/ManageKafkaPermissions/components/ResourcePrefix.tsx
+++ src/Kafka/ManageKafkaPermissions/components/ResourcePrefix.tsx
@@ -1,38 +1,90 @@
-import { FormGroup, TextInput, ValidatedOptions } from "@patternfly/react-core";
+import {
+  FormGroup,
+  Select,
+  SelectOption,
+  SelectProps,
+  SelectVariant,
+  ValidatedOptions,
+} from "@patternfly/react-core";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export type ResourcePrefixProps = {
   value: string | undefined;
-  onChangeValue: (value: string | undefined) => void;
+  onChange: (value: string | undefined) => void;
   invalid: boolean;
+  resourceCondition: "Starts with" | "Is";
+  onFetchOptions: () => Promise<string[]>;
 };
 
 export const ResourcePrefix: React.VFC<ResourcePrefixProps> = ({
   value,
-  onChangeValue,
+  onChange,
   invalid,
+  resourceCondition,
+  onFetchOptions,
 }) => {
   const { t } = useTranslation(["manage-kafka-permissions"]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [typeAheadSuggestions, setTypeAheadSuggestions] = useState<string[]>(
+    []
+  );
+  const [filter, setFilter] = useState<string>();
 
-  const onPrefixValueChange = (value: string | undefined) => {
-    onChangeValue(value);
+  useEffect(() => {
+    onFetchOptions().then((results: string[]) =>
+      setTypeAheadSuggestions(results)
+    );
+  }, [onFetchOptions, filter]);
+
+  const onSelect: SelectProps["onSelect"] = (_, value) => {
+    onChange(value as string);
+  };
+  const onToggle = (newState: boolean) => {
+    setIsOpen(newState);
+  };
+  const clearSelection = () => {
+    onChange(undefined);
+    setIsOpen(false);
+  };
+  const makeOptions = () => {
+    return typeAheadSuggestions.map((value, index) => (
+      <SelectOption key={index} value={value}>
+        {value}
+      </SelectOption>
+    ));
   };
 
   return (
     <FormGroup
       validated={invalid ? ValidatedOptions.error : ValidatedOptions.default}
       helperTextInvalid={t("common:required")}
-      fieldId={"resource-prefix-input"}
+      fieldId={"resource-prefix-select"}
     >
-      <TextInput
-        placeholder={t("resourcePrefix.placeholder_text")}
+      <Select
+        id={"resource-prefix-select"}
+        data-testid="acls-prefix-select"
+        variant={SelectVariant.typeahead}
+        typeAheadAriaLabel={t("resourcePrefix.prefix_aria_label")}
+        onToggle={onToggle}
+        onSelect={onSelect}
+        onClear={clearSelection}
+        selections={value}
+        isOpen={isOpen}
+        placeholderText={
+          resourceCondition === "Starts with"
+            ? t("resourcePrefix.placeholder_text_starts_with")
+            : t("resourcePrefix.placeholder_text_is")
+        }
+        isCreatable={false}
+        menuAppendTo="parent"
         validated={invalid ? ValidatedOptions.error : ValidatedOptions.default}
-        value={value}
-        type="text"
-        onChange={onPrefixValueChange}
-        aria-label="Prefix value text input"
-        style={{ maxWidth: 200 }}
-      />
+        maxHeight={400}
+        width={200}
+        onTypeaheadInputChanged={(value) => setFilter(value)}
+      >
+        {makeOptions()}
+      </Select>
     </FormGroup>
   );
 };
