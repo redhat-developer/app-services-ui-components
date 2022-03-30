2aeac877c746f36b04f706121a42dd7d1d2e9c59
Riccardo Forina <rforina@redhat.com>
2022-03-30 15:06:46 +0200

    Fix metrics tooltip (#245)
    * fix: use the chart title for the generic chart component as the tooltip
    
    Also, adds a missing tooltip in the used disk space chart title
    
    * add some tests

--- src/Kafka/Metrics/components/ChartSkeletonLoader.tsx
+++ src/Kafka/Metrics/components/ChartSkeletonLoader.tsx
@@ -1,17 +1,24 @@
 import { Flex, FlexItem, Skeleton } from "@patternfly/react-core";
 import { VoidFunctionComponent } from "react";
 import { chartHeight, chartPadding } from "../consts";
+import { useTranslation } from "react-i18next";
 
-export const ChartSkeletonLoader: VoidFunctionComponent = () => (
-  <Flex direction={{ default: "column" }} data-chromatic="ignore">
-    <FlexItem>
-      <Skeleton height={`${chartHeight - chartPadding.bottom}px`} />
-    </FlexItem>
-    <FlexItem>
-      <Skeleton height={`${chartPadding.bottom / 2 - 12.5}px`} width="20%" />
-    </FlexItem>
-    <FlexItem>
-      <Skeleton height={`${chartPadding.bottom / 2 - 12.5}px`} width="40%" />
-    </FlexItem>
-  </Flex>
-);
+export const ChartSkeletonLoader: VoidFunctionComponent = () => {
+  const { t } = useTranslation();
+  return (
+    <Flex direction={{ default: "column" }} data-chromatic="ignore">
+      <FlexItem>
+        <Skeleton
+          height={`${chartHeight - chartPadding.bottom}px`}
+          screenreaderText={t("common:skeleton_loader_screenreader_text")}
+        />
+      </FlexItem>
+      <FlexItem>
+        <Skeleton height={`${chartPadding.bottom / 2 - 12.5}px`} width="20%" />
+      </FlexItem>
+      <FlexItem>
+        <Skeleton height={`${chartPadding.bottom / 2 - 12.5}px`} width="40%" />
+      </FlexItem>
+    </Flex>
+  );
+};
