d325325980216dcff879051b4094c5f409353845
hemahg <hhg@redhat.com>
2022-03-31 22:28:48 +0530

    Add test

---
+++ src/Kafka/ConsumerGroups/components/ConsumerGroupEmptyState.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ConsumerGroupEmptyState } from "./ConsumerGroupEmptyState";
+
+export default {
+  component: ConsumerGroupEmptyState,
+  args: {},
+  parameters: {
+    backgrounds: {
+      default: "Background color 100",
+    },
+  },
+} as ComponentMeta<typeof ConsumerGroupEmptyState>;
+
+const Template: ComponentStory<typeof ConsumerGroupEmptyState> = (args) => (
+  <ConsumerGroupEmptyState {...args} />
+);
+
+export const EmptyState = Template.bind({});
--- src/Kafka/ConsumerGroups/components/index.ts
+++ src/Kafka/ConsumerGroups/components/index.ts
@@ -6,3 +6,4 @@ export * from "./ConsumerGroupTable";
 export * from "./ConsumerGroupState";
 export * from "./ConsumerGroupPagination";
 export * from "./ConsumerGroupToolbar";
+export * from "./ConsumerGroupEmptyState";
