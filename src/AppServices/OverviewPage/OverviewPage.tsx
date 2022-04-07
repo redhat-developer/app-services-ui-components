cf84ab0d29f156b8eea2518ac87cacc932910b2f
hemahg <hhg@redhat.com>
2022-04-07 17:52:05 +0530

    Update cards as per review comments

--- src/AppServices/OverviewPage/OverviewPage.tsx
+++ src/AppServices/OverviewPage/OverviewPage.tsx
@@ -132,20 +132,20 @@ export const OverviewPage: FunctionComponent<OverviewPageProps> = ({
             </CardFooter>
           </Card>
 
-          {/* Connectors card */}
-          <Card>
+          {/* Kafka card */}
+          <Card ouiaId="card-rhosak">
             <CardHeader>
               <CardHeaderMain>
                 <img
-                  src={LogoRedHatOpenShiftConnectorsBStandardRgb}
-                  alt="Red Hat OpenShift Connectors logo"
+                  src={LogoRedHatOpenShiftStreamsForApacheKafkaAStandardRgbPng}
+                  alt="Red Hat OpenShift Streams for Apache Kafka logo"
                   style={{ height: "50px" }}
                 />
               </CardHeaderMain>
             </CardHeader>
             <CardTitle>
               <Title headingLevel="h2" size="xl">
-                {t("overview:rhocTitle")}
+                {t("overview:rhosakTitle")}
               </Title>
             </CardTitle>
             <CardBody>
@@ -155,91 +155,23 @@ export const OverviewPage: FunctionComponent<OverviewPageProps> = ({
                     <Label color="blue">
                       {t("overview:applicationService")}
                     </Label>
-                    <Label>{t("overview:servicePreview")}</Label>
-                  </LabelGroup>
-                </StackItem>
-                <StackItem>{t("overview:rhocMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview:rhocSecondaryText")}{" "}
-                  <Button
-                    data-testid="cardRHOC-linkOpenShift"
-                    isInline
-                    variant={ButtonVariant.link}
-                    component="a"
-                    target="_blank"
-                    href="https://openshift.com"
-                  >
-                    OpenShift
-                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
-                  </Button>
-                  {t("overview:rhocSecondaryTextContinued")}
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHOC-buttonCTA"
-                variant={ButtonVariant.secondary}
-                target="_blank"
-                component={(props) => <Link {...props} to={toConnectorsHref} />}
-              >
-                {t("overview:getStarted")}
-              </Button>
-            </CardFooter>
-          </Card>
-
-          {/* Data science card */}
-          <Card>
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftDataScienceAStandardRgb}
-                  alt="Red Hat OpenShift Data Science logo"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview:rhodsTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem className="pf-u-mb-lg">
-                  <LabelGroup>
                     <Label color="green">{t("overview:dataService")}</Label>
-                    <Label>{t("overview:fieldTrial")}</Label>
                   </LabelGroup>
                 </StackItem>
-                <StackItem>{t("overview:rhodsMainText")}</StackItem>
+                <StackItem>{t("overview:rhosakMainText")}</StackItem>
                 <StackItem className="pf-u-color-200">
-                  {t("overview:rhodsSecondaryText")}{" "}
-                  <Button
-                    data-testid="cardRHODS-linkOpenShift"
-                    isInline
-                    variant={ButtonVariant.link}
-                    component="a"
-                    target="_blank"
-                    href="https://openshift.com"
-                  >
-                    {" "}
-                    OpenShift
-                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
-                  </Button>
-                  .
+                  {t("overview:rhosakSecondaryText")}
                 </StackItem>
               </Stack>
             </CardBody>
             <CardFooter>
               <Button
-                data-testid="cardRHODS-buttonTryIt"
-                ouiaId="button-rhods-tryit"
+                data-testid="cardRHOSAK-buttonCreateKafka"
                 variant={ButtonVariant.secondary}
-                component="a"
-                target="_blank"
+                component={(props) => <Link {...props} to={toKafkaHref} />}
+                ouiaId="button-create"
               >
-                {t("overview:tryIt")}
+                {t("overview:rhosakCallToActionButton")}
               </Button>
             </CardFooter>
           </Card>
@@ -292,20 +224,20 @@ export const OverviewPage: FunctionComponent<OverviewPageProps> = ({
             </CardFooter>
           </Card>
 
-          {/* Kafka card */}
-          <Card ouiaId="card-rhosak">
+          {/* Connectors card */}
+          <Card>
             <CardHeader>
               <CardHeaderMain>
                 <img
-                  src={LogoRedHatOpenShiftStreamsForApacheKafkaAStandardRgbPng}
-                  alt="Red Hat OpenShift Streams for Apache Kafka logo"
+                  src={LogoRedHatOpenShiftConnectorsBStandardRgb}
+                  alt="Red Hat OpenShift Connectors logo"
                   style={{ height: "50px" }}
                 />
               </CardHeaderMain>
             </CardHeader>
             <CardTitle>
               <Title headingLevel="h2" size="xl">
-                {t("overview:rhosakTitle")}
+                {t("overview:rhocTitle")}
               </Title>
             </CardTitle>
             <CardBody>
@@ -315,22 +247,35 @@ export const OverviewPage: FunctionComponent<OverviewPageProps> = ({
                     <Label color="blue">
                       {t("overview:applicationService")}
                     </Label>
+                    <Label>{t("overview:servicePreview")}</Label>
                   </LabelGroup>
                 </StackItem>
-                <StackItem>{t("overview:rhosakMainText")}</StackItem>
+                <StackItem>{t("overview:rhocMainText")}</StackItem>
                 <StackItem className="pf-u-color-200">
-                  {t("overview:rhosakSecondaryText")}
+                  {t("overview:rhocSecondaryText")}{" "}
+                  <Button
+                    data-testid="cardRHOC-linkOpenShift"
+                    isInline
+                    variant={ButtonVariant.link}
+                    component="a"
+                    target="_blank"
+                    href="https://openshift.com"
+                  >
+                    OpenShift
+                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
+                  </Button>
+                  {t("overview:rhocSecondaryTextContinued")}
                 </StackItem>
               </Stack>
             </CardBody>
             <CardFooter>
               <Button
-                data-testid="cardRHOSAK-buttonCreateKafka"
+                data-testid="cardRHOC-buttonCTA"
                 variant={ButtonVariant.secondary}
-                component={(props) => <Link {...props} to={toKafkaHref} />}
-                ouiaId="button-create"
+                target="_blank"
+                component={(props) => <Link {...props} to={toConnectorsHref} />}
               >
-                {t("overview:rhosakCallToActionButton")}
+                {t("overview:getStarted")}
               </Button>
             </CardFooter>
           </Card>
@@ -388,6 +333,62 @@ export const OverviewPage: FunctionComponent<OverviewPageProps> = ({
               >
                 {t("overview:learnMore")}{" "}
                 <ExternalLinkAltIcon className="pf-u-ml-sm" />
+              </Button>
+            </CardFooter>
+          </Card>
+
+          {/* Data science card */}
+          <Card>
+            <CardHeader>
+              <CardHeaderMain>
+                <img
+                  src={LogoRedHatOpenShiftDataScienceAStandardRgb}
+                  alt="Red Hat OpenShift Data Science logo"
+                  style={{ height: "50px" }}
+                />
+              </CardHeaderMain>
+            </CardHeader>
+            <CardTitle>
+              <Title headingLevel="h2" size="xl">
+                {t("overview:rhodsTitle")}
+              </Title>
+            </CardTitle>
+            <CardBody>
+              <Stack hasGutter>
+                <StackItem className="pf-u-mb-lg">
+                  <LabelGroup>
+                    <Label color="green">{t("overview:dataService")}</Label>
+                    <Label>{t("overview:fieldTrial")}</Label>
+                  </LabelGroup>
+                </StackItem>
+                <StackItem>{t("overview:rhodsMainText")}</StackItem>
+                <StackItem className="pf-u-color-200">
+                  {t("overview:rhodsSecondaryText")}{" "}
+                  <Button
+                    data-testid="cardRHODS-linkOpenShift"
+                    isInline
+                    variant={ButtonVariant.link}
+                    component="a"
+                    target="_blank"
+                    href="https://openshift.com"
+                  >
+                    {" "}
+                    OpenShift
+                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
+                  </Button>
+                  .
+                </StackItem>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <Button
+                data-testid="cardRHODS-buttonTryIt"
+                ouiaId="button-rhods-tryit"
+                variant={ButtonVariant.secondary}
+                component="a"
+                target="_blank"
+              >
+                {t("overview:tryIt")}
               </Button>
             </CardFooter>
           </Card>
