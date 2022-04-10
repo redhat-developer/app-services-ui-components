2be11658039c05961e6b6d965e1a75405f5ebebf
Sim Dhaliwal <sdhaliwa@redhat.com>
2022-04-10 19:19:31 -0400

    Ran formatting

--- src/ProofOfConcepts/MetricsLag/MetricsLagAlert.tsx
+++ src/ProofOfConcepts/MetricsLag/MetricsLagAlert.tsx
@@ -2,30 +2,30 @@ import { VoidFunctionComponent } from "react";
 import React from "react";
 
 import {
-    Alert,
-    AlertActionCloseButton,
-    AlertActionLink
+  Alert,
+  AlertActionCloseButton,
+  AlertActionLink,
 } from "@patternfly/react-core";
 
 export const MetricsLagAlert: VoidFunctionComponent = () => {
   return (
     <React.Fragment>
       <Alert
-      isInline
-      variant="info"
-      title="Metrics experience lag"
-      actionClose={
-        <AlertActionCloseButton
-          onClose={() => alert("Clicked the close button")}
-        />
-      }
-    >
-      <p>
-        Metrics regularly experience lag, and do not automatically refresh. This
-        might result in metrics appearing out-of-sync and with details displayed
-        on other pages.
-      </p>
-    </Alert>
+        isInline
+        variant="info"
+        title="Metrics experience lag"
+        actionClose={
+          <AlertActionCloseButton
+            onClose={() => alert("Clicked the close button")}
+          />
+        }
+      >
+        <p>
+          Metrics regularly experience lag, and do not automatically refresh.
+          This might result in metrics appearing out-of-sync and with details
+          displayed on other pages.
+        </p>
+      </Alert>
     </React.Fragment>
   );
 };
