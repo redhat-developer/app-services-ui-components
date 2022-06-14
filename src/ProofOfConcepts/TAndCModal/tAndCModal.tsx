c011cb37719ccf10ee4dcf4ca1a4195be1db3d8f
Kellie O Donovan <120460002@umail.ucc.ie>
2022-06-14 10:20:27 +0100

    chore: POC add modal to warn user when they are leaving to accept T&C

---
+++ src/ProofOfConcepts/TAndCModal/tAndCModal.tsx
@@ -0,0 +1,61 @@
+import React, { VoidFunctionComponent } from "react";
+import { Modal, ModalVariant, Button } from "@patternfly/react-core";
+
+var linebreak = "\n";
+
+export class SmallModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.handleModalToggle = () => {
+      this.setState(({ isModalOpen }) => ({
+        isModalOpen: !isModalOpen,
+      }));
+    };
+  }
+
+  render() {
+    const { isModalOpen } = this.state;
+
+    return (
+      <React.Fragment>
+        <Button variant="primary" onClick={this.handleModalToggle}>
+          Show small modal
+        </Button>
+        <Modal
+          id="modalTerms"
+          variant={ModalVariant.small}
+          title="Terms and Conditions"
+          isOpen={isModalOpen}
+          onClose={this.handleModalToggle}
+          actions={[
+            <Button
+              key="confirm"
+              variant="primary"
+              data-testid="actionViewTerms"
+              onClick={this.handleModalToggle}
+            >
+              View Terms and Conditions
+            </Button>,
+            <Button
+              key="cancel"
+              variant="link"
+              data-testid="actionCancelViewTerms"
+              onClick={this.handleModalToggle}
+            >
+              Cancel
+            </Button>,
+          ]}
+        >
+          <div>Red Hat has Terms and Conditions for its managed services.</div>
+          <div class="pf-u-font-weight-bold">
+            If you do not accept the terms, you will not be able to create new
+            Kafka instances.
+          </div>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
