f5182258185ccbe063352f5407709197eb9a8c73
Kells512 <72694484+Kells512@users.noreply.github.com>
2022-07-04 16:42:04 +0100

    chore: POC add modal to warn user when they are leaving to accept T&C (#417)
    * chore: POC add modal to warn user when they are leaving to accept T&C
    
    * Adjusted layout in the modal
    
    * Adjust layout in the modal - some comments taken out
    
    * Changed after comments

---
+++ src/ProofOfConcepts/TAndCModal/tAndCModal.tsx
@@ -0,0 +1,68 @@
+import React, { VoidFunctionComponent } from "react";
+import { Modal, ModalVariant, Button } from "@patternfly/react-core";
+import { Flex, FlexItem } from "@patternfly/react-core";
+
+export class SmallModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.handleModalToggle = () => {
+      this.setState(({ isModalOpen }) => ({
+        isModalOpen: !isModalOpen,
+      }));
+    };
+  }
+
+  render() {
+    const { isModalOpen } = this.state;
+
+    return (
+      <React.Fragment>
+        <Button variant="primary" onClick={this.handleModalToggle}>
+          Show small modal
+        </Button>
+        <Modal
+          id="modalTerms"
+          variant={ModalVariant.small}
+          title="Terms and Conditions"
+          isOpen={isModalOpen}
+          onClose={this.handleModalToggle}
+          actions={[
+            <Button
+              key="confirm"
+              variant="primary"
+              data-testid="actionViewTerms"
+              onClick={this.handleModalToggle}
+            >
+              View Terms and Conditions
+            </Button>,
+            <Button
+              key="cancel"
+              variant="link"
+              data-testid="actionCancelViewTerms"
+              onClick={this.handleModalToggle}
+            >
+              Cancel
+            </Button>,
+          ]}
+        >
+          <Flex direction={{ default: "column" }}>
+            <FlexItem>
+              <p>Red Hat has Terms and Conditions for its managed services.</p>
+            </FlexItem>
+            <FlexItem>
+              <p>
+                <strong>
+                  If you do not accept the terms, you will not be able to create
+                  new Kafka instances.
+                </strong>
+              </p>
+            </FlexItem>
+          </Flex>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
