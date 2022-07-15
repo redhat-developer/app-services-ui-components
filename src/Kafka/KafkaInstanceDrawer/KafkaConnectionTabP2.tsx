d6e5e96f2bd9f7c89859410aa3bcbf5a41bdab19
Kellie O Donovan <120460002@umail.ucc.ie>
2022-07-15 11:53:58 +0100

    fix checks
    Signed-off-by: Kellie O Donovan <120460002@umail.ucc.ie>

--- src/Kafka/KafkaInstanceDrawer/KafkaConnectionTabP2.tsx
+++ src/Kafka/KafkaInstanceDrawer/KafkaConnectionTabP2.tsx
@@ -155,6 +155,7 @@ export const KafkaConnectionTabP2: FunctionComponent<
                           <ExternalLink
                             testId={"customerPortal-link"}
                             href={linkToDocPortal}
+                            className="pf-u-ml-xs"
                           />,
                         ]}
                       />
@@ -214,6 +215,7 @@ export const KafkaConnectionTabP2: FunctionComponent<
                           <ExternalLink
                             testId={"customerPortal-link"}
                             href={linkToDocPortal}
+                            className="pf-u-ml-xs"
                           />,
                         ]}
                       />
