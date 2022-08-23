ccd2bebd8edab0b7ceea6e90419ccdd7427a2722
HemaHG <53568062+HemaHG@users.noreply.github.com>
2022-08-23 10:12:58 +0530

    feat: new version of connection tab to update API swagger documentation (#508)

--- .storybook/withMas.tsx
+++ .storybook/withMas.tsx
@@ -45,6 +45,8 @@ export const withMas = (
               "overview-v3": () => import("../locales/en/overview-v3.json"),
               "connection-tab": () =>
                 import("../locales/en/connection-tab.json"),
+              "connection-tab-p1": () =>
+                import("../locales/en/connection-tab-p1.json"),
             },
             it: {
               common: () => Promise.resolve({ delete: "Elimina" }),
