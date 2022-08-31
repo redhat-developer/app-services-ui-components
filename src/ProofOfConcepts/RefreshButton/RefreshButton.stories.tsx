d0a8fa91d840902c6df5ebcb3cd3f57955a08f0b
Kellie O Donovan <120460002@umail.ucc.ie>
2022-08-31 09:57:28 +0100

    chore: Refresh Button

---
+++ src/ProofOfConcepts/RefreshButton/RefreshButton.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Toolbar, ToolbarContent } from "@patternfly/react-core";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import { sub } from "date-fns";
+
+import { POCRefreshButton } from "./RefreshButton";
+
+export default {
+  component: POCRefreshButton,
+  args: {
+    isRefreshing: false,
+    lastUpdated: sub(new Date(), { minutes: 3 }),
+  },
+} as ComponentMeta<typeof POCRefreshButton>;
+
+const Template: ComponentStory<typeof POCRefreshButton> = (args) => (
+  <Toolbar>
+    <ToolbarContent>
+      <POCRefreshButton {...args} />
+    </ToolbarContent>
+  </Toolbar>
+);
+
+export const Story = Template.bind({});
+Story.args = {};
