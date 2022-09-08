85ee2e8d57414a7e347658191688d079f23782eb
suyash-naithani <66817941+suyash-naithani@users.noreply.github.com>
2022-09-08 17:45:25 +0530

    chore: Create topic wizard (#519)
    * chore: Create topic wizard
    
    Co-authored-by: Riccardo Forina <rforina@redhat.com>

---
+++ src/Kafka/CreateKafkaTopic/components/CustomRetentionSize.tsx
@@ -0,0 +1,95 @@
+import type React from "react";
+import {
+  Flex,
+  FlexItem,
+  Select,
+  SelectOption,
+  NumberInput,
+  SelectVariant,
+} from "@patternfly/react-core";
+import type { NumberInputProps, SelectProps } from "@patternfly/react-core";
+import type { NewTopic, RetentionSizeUnits, SelectOptions } from "../types";
+
+export type CustomRetentionSizeProps = NumberInputProps &
+  SelectProps & {
+    id?: string;
+    selectOptions: SelectOptions[];
+    topicData: NewTopic;
+    setTopicData: (data: NewTopic) => void;
+  };
+
+const CustomRetentionSize: React.FC<CustomRetentionSizeProps> = ({
+  onToggle,
+  isOpen,
+  selectOptions,
+  topicData,
+  setTopicData,
+}) => {
+  const onSelect: SelectProps["onSelect"] = (event, value) => {
+    setTopicData({
+      ...topicData,
+      customRetentionSizeUnit: value as RetentionSizeUnits,
+    });
+
+    //}
+
+    onToggle(false, event);
+  };
+
+  const handleTouchSpin = (operator: string) => {
+    if (operator === "+") {
+      setTopicData({
+        ...topicData,
+        retentionBytes: topicData.retentionBytes + 1,
+      });
+    } else if (operator === "-") {
+      setTopicData({
+        ...topicData,
+        retentionBytes: topicData.retentionBytes - 1,
+      });
+    }
+  };
+
+  const onChangeTouchSpin = (event: React.FormEvent<HTMLInputElement>) => {
+    setTopicData({
+      ...topicData,
+      retentionBytes: Number(event.currentTarget.value),
+    });
+  };
+
+  return (
+    <div className="kafka-ui--radio__parameters">
+      <Flex>
+        <FlexItem>
+          <NumberInput
+            onMinus={() => handleTouchSpin("-")}
+            onPlus={() => handleTouchSpin("+")}
+            value={topicData.retentionBytes}
+            onChange={(event) => onChangeTouchSpin(event)}
+            min={0}
+          />
+        </FlexItem>
+        <FlexItem>
+          <Select
+            variant={SelectVariant.single}
+            aria-label="Select Input"
+            onToggle={onToggle}
+            onSelect={onSelect}
+            selections={topicData.customRetentionSizeUnit}
+            isOpen={isOpen}
+          >
+            {selectOptions?.map((s) => (
+              <SelectOption
+                key={s.key}
+                value={s.value}
+                isPlaceholder={s.isPlaceholder}
+              />
+            ))}
+          </Select>
+        </FlexItem>
+      </Flex>
+    </div>
+  );
+};
+
+export { CustomRetentionSize };
---
+++ src/shared/DropdownWithToggle/DropdownWithToggle.tsx
@@ -0,0 +1,92 @@
+import type React from "react";
+import { useState } from "react";
+import { Dropdown, DropdownItem, DropdownToggle } from "@patternfly/react-core";
+import type { DropdownProps } from "@patternfly/react-core/dist/js";
+
+export interface IDropdownWithToggleProps {
+  id: string;
+  toggleId: string;
+  value: string;
+  name: string;
+  items: IDropdownOption[];
+  onSelectOption?: (value: string, name: string) => void;
+  ariaLabel?: string;
+  menuAppendTo?: HTMLElement | (() => HTMLElement) | "parent" | "inline";
+  isLabelAndValueNotSame?: boolean;
+}
+
+export interface IDropdownOption {
+  value?: string;
+  label?: string;
+  key?: string;
+  isDisabled?: boolean;
+}
+
+export const DropdownWithToggle: React.FC<IDropdownWithToggleProps> = ({
+  id,
+  toggleId,
+  items,
+  value,
+  ariaLabel,
+  onSelectOption,
+  name,
+  menuAppendTo,
+  isLabelAndValueNotSame,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>();
+
+  const onToggle = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
+
+  const onSelect: DropdownProps["onSelect"] = (e) => {
+    if (e && e.currentTarget.textContent) {
+      const value: string = e.currentTarget.textContent;
+      if (onSelectOption && value) {
+        onSelectOption(value.toLowerCase(), name);
+      }
+      setIsOpen((isOpen) => !isOpen);
+    }
+  };
+
+  const getItems = (options: IDropdownOption[]) => {
+    const items = options.map((option) => {
+      const { key, value, label } = option;
+
+      return (
+        <DropdownItem key={key} value={value}>
+          {label || value}
+        </DropdownItem>
+      );
+    });
+
+    return items;
+  };
+
+  const getSelectedValue = () => {
+    if (isLabelAndValueNotSame) {
+      const filteredOption = items?.filter((item) => item.value === value)[0];
+      return filteredOption?.label;
+    }
+    return value;
+  };
+
+  const dropdownToggle = (
+    <DropdownToggle id={toggleId} onToggle={onToggle}>
+      {getSelectedValue()}
+    </DropdownToggle>
+  );
+
+  return (
+    <Dropdown
+      name={name}
+      id={id}
+      onSelect={onSelect}
+      toggle={dropdownToggle}
+      isOpen={isOpen}
+      aria-label={ariaLabel}
+      dropdownItems={getItems(items)}
+      menuAppendTo={menuAppendTo}
+    />
+  );
+};
