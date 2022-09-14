3ab3d79817757d10e0fc22b545f5bea09e739b76
suyash-naithani <suyash.naithani@gmail.com>
2022-09-14 21:12:45 +0530

    Export the create topic page
    Signed-off-by: suyash-naithani <suyash.naithani@gmail.com>

--- src/Kafka/CreateKafkaTopic/components/index.ts
+++ src/Kafka/CreateKafkaTopic/components/index.ts
@@ -7,3 +7,4 @@ export * from "./StepTopicName";
 export * from "./WizardCustomFooter";
 export * from "./TopicAdvancePage";
 export * from "./PartitionLimitWarning";
+export * from "./CreateTopicPage";
