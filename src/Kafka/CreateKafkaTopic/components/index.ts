503259ad041eb10d82e4ca093452d0b9005e1c92
suyash-naithani <suyash.naithani@gmail.com>
2022-09-14 19:28:41 +0530

    Export types

--- src/Kafka/CreateKafkaTopic/components/index.ts
+++ src/Kafka/CreateKafkaTopic/components/index.ts
@@ -5,6 +5,5 @@ export * from "./StepPartitions";
 export * from "./StepReplicas";
 export * from "./StepTopicName";
 export * from "./WizardCustomFooter";
-export * from "./CreateTopicWizard";
 export * from "./TopicAdvancePage";
 export * from "./PartitionLimitWarning";
