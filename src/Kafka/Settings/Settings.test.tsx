4bad5f15e3650d10a513f9f14a55bb8be3024b01
hemahg <hhg@redhat.com>
2022-09-14 20:55:43 +0530

    fix test
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/Kafka/Settings/Settings.test.tsx
+++ src/Kafka/Settings/Settings.test.tsx
@@ -3,8 +3,11 @@ import { composeStories } from "@storybook/testing-react";
 import * as stories from "./Settings.stories";
 import { userEvent } from "@storybook/testing-library";
 
-const { TurningOnConnectionReauthentication, TurnOnConnectionFailure } =
-  composeStories(stories);
+const {
+  TurningOnConnectionReauthentication,
+  TurnOnConnectionFailure,
+  InteractiveExample,
+} = composeStories(stories);
 
 describe("Settings", () => {
   it("Connection re-authentication turned on", async () => {
@@ -13,40 +16,85 @@ describe("Settings", () => {
 
     expect(
       comp.getByRole("checkbox", { name: "Connection re-authentication" })
-    ).toBeChecked();
-    expect(
-      await comp.findByText("Connection re-authentication turned on")
-    ).toBeInTheDocument();
+    ).not.toBeChecked();
 
     userEvent.click(
-      comp.getByRole("button", {
-        name: "Success Alert close",
-      })
+      await comp.getByRole("checkbox", { name: "Connection re-authentication" })
     );
-    expect(
-      comp.queryByText("Connection re-authentication turned on")
-    ).not.toBeInTheDocument();
+    setTimeout(() => {
+      expect(
+        comp.findByText("Connection re-authentication turned on")
+      ).toBeInTheDocument();
+
+      userEvent.click(
+        comp.getByRole("button", {
+          name: "Success Alert close",
+        })
+      );
+
+      expect(
+        comp.queryByText("Connection re-authentication turned on")
+      ).not.toBeInTheDocument();
+
+      expect(
+        comp.getByRole("checkbox", { name: "Connection re-authentication" })
+      ).toBeChecked();
+    }, 4000);
   });
 
   it("Connection re-authentication updation failed", async () => {
     const comp = render(<TurnOnConnectionFailure />);
     await waitForI18n(comp);
-    expect(await comp.findByText("Something went wrong")).toBeInTheDocument();
-    expect(
-      await comp.findByText(
-        "We're unable to update connection re-authentication at this time. Try again later."
-      )
-    ).toBeInTheDocument();
-
     userEvent.click(
-      comp.getByRole("button", {
-        name: "Close Danger alert: alert: Something went wrong",
-      })
+      await comp.getByRole("checkbox", { name: "Connection re-authentication" })
+    );
+    setTimeout(() => {
+      expect(
+        comp.findByText(
+          "We're unable to update connection re-authentication at this time. Try again later."
+        )
+      ).toBeInTheDocument();
+
+      userEvent.click(
+        comp.getByRole("button", {
+          name: "Close Danger alert: alert: Something went wrong",
+        })
+      );
+      expect(
+        comp.queryByText(
+          "We're unable to update connection re-authentication at this time. Try again later."
+        )
+      ).not.toBeInTheDocument();
+    }, 4000);
+  });
+
+  it("Connection re-authentication turned off", async () => {
+    const comp = render(<InteractiveExample />);
+    await waitForI18n(comp);
+
+    expect(
+      comp.getByRole("checkbox", { name: "Connection re-authentication" })
+    ).toBeChecked();
+    userEvent.click(
+      await comp.getByRole("checkbox", { name: "Connection re-authentication" })
     );
     expect(
-      comp.queryByText(
-        "We're unable to update connection re-authentication at this time. Try again later."
+      await comp.findByText("Turn off re-authentication?")
+    ).toBeInTheDocument();
+    expect(
+      await comp.findByText(
+        "There are potential risks associated with turning off re-authentication, where attackers could stay connected indefinitely."
       )
-    ).not.toBeInTheDocument();
+    ).toBeInTheDocument();
+    userEvent.click(comp.getByRole("button", { name: "Turn off" }));
+    setTimeout(() => {
+      expect(
+        comp.findByText("Connection re-authentication turned off")
+      ).toBeInTheDocument();
+
+      expect(
+        comp.getByRole("checkbox", { name: "Connection re-authentication" })
+      ).not.toBeChecked();
+    }, 4000);
   });
 });
