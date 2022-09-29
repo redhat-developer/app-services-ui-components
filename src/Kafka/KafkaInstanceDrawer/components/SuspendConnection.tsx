fc9d08057e76621a5f577445249cdf6f87b7b388
hemahg <hhg@redhat.com>
2022-09-29 20:00:36 +0530

    Create UI components to communicate the Kafka instance is suspended
    Signed-off-by: hemahg <hhg@redhat.com>

---
+++ src/Kafka/KafkaInstanceDrawer/components/SuspendConnection.tsx
@@ -0,0 +1,24 @@
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  EmptyStateVariant,
+  Title,
+  TitleSizes,
+} from "@patternfly/react-core";
+import { ExclamationTriangleIcon } from "@patternfly/react-icons";
+import type { VoidFunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
+
+export const SuspendConnection: VoidFunctionComponent = () => {
+  const { t } = useTranslation("kafka");
+  return (
+    <EmptyState variant={EmptyStateVariant.large}>
+      <EmptyStateIcon icon={ExclamationTriangleIcon} color={"#f0ab00"} />
+      <Title headingLevel={"h1"} size={TitleSizes.lg}>
+        {t("suspend_empty_state_title")}
+      </Title>
+      <EmptyStateBody>{t("suspend_empty_state_body")}</EmptyStateBody>
+    </EmptyState>
+  );
+};
