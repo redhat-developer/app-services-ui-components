aee2ee663e74ec8a342463aafd3797cf9a1f4a07
hemahg <hhg@redhat.com>
2022-12-12 16:33:27 +0530

    Use shared table
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/Kafka/ConsumerGroups/components/index.ts
+++ src/Kafka/ConsumerGroups/components/index.ts
@@ -2,7 +2,6 @@ export * from "./ConsumerGroupDetails";
 export * from "./ConsumerGroupPopover";
 export * from "./ConsumerGroupByKafka";
 export * from "./ConsumerGroupByTopic";
-export * from "./ConsumerGroupTable";
 export * from "./ConsumerGroupState";
 export * from "./ConsumerGroupToolbar";
 export * from "./ConsumerGroupEmptyState";
