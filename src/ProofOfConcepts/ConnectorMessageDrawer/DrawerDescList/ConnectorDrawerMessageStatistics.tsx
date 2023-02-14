8be5fd50d1a078a2efe14db6e321c69ab8e3b6be
Kellie O Donovan <120460002@umail.ucc.ie>
2023-02-14 11:53:25 +0000

    Drawer bug

--- src/ProofOfConcepts/ConnectorMessageDrawer/DrawerDescList/ConnectorDrawerMessageStatistics.tsx
+++ src/ProofOfConcepts/ConnectorMessageDrawer/DrawerDescList/ConnectorDrawerMessageStatistics.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent } from "react";
 
 import {
   Alert,
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -20,6 +21,8 @@ import {
   DescriptionList,
   DescriptionListGroup,
   DescriptionListDescription,
+  Drawer,
+  DrawerContent,
 } from "@patternfly/react-core";
 
 import { ExclamationIcon } from "@patternfly/react-icons";
@@ -29,75 +32,77 @@ import "./ConnectorDrawer.css";
 
 export const ConnectorDrawerMessageStatistics = () => {
   return (
-    <Flex direction={{ default: "column" }}>
-      <FlexItem>
-        <Alert
-          variant="warning"
-          isInline
-          title={
-            "These numbers reflect the messages in the last 23 hours 37 minutes"
-          }
-        />
-      </FlexItem>
-      <FlexItem>
-        <Card>
-          <CardTitle>
-            <Title headingLevel="h3" size={TitleSizes["lg"]}>
-              Processed messages
-            </Title>
-          </CardTitle>
-          {/** split, grid, --desc list */}
-          <CardBody className="sent-spacing">
-            <DescriptionList
-              columnModifier={{ default: "3Col" }}
-              isInlineGrid
-              horizontalTermWidthModifier={{
-                default: "35ch",
-              }}
-            >
-              <DescriptionListGroup>
-                <DescriptionListDescription>
-                  <CheckIcon className="pf-u-icon-color-green" />
-                  1600 sent
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              <DescriptionListGroup>
-                <DescriptionListDescription>
-                  <ExclamationIcon className="pf-u-icon-color-red" />
-                  12 not sent
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-            </DescriptionList>
-          </CardBody>
-          <CardFooter className="processed-messages-info">
-            These numbers reflect the messages processed in the last 24 hours
-          </CardFooter>
-        </Card>
-      </FlexItem>
-      <FlexItem>
-        <TextContent>
-          <TextList component={TextListVariants.dl}>
-            <TextListItem component={TextListItemVariants.dt}>
-              Error handling method
-            </TextListItem>
-            <TextListItem component={TextListItemVariants.dd}>
-              Dead letter queue
-            </TextListItem>
-            <TextListItem component={TextListItemVariants.dt}>
-              Dead letter queue topic
-            </TextListItem>
-            <TextListItem
-              className="pf-u-link-color"
-              component={TextListItemVariants.dd}
-            >
-              my-topic-2
-            </TextListItem>
-          </TextList>
-        </TextContent>
-      </FlexItem>
-      <FlexItem>
-        <Divider />
-      </FlexItem>
-    </Flex>
+    <React.Fragment>
+      <Flex direction={{ default: "column" }}>
+        <FlexItem>
+          <Alert
+            variant="warning"
+            isInline
+            title={
+              "These numbers reflect the messages in the last 23 hours 37 minutes"
+            }
+          />
+        </FlexItem>
+        <FlexItem>
+          <Card>
+            <CardTitle>
+              <Title headingLevel="h3" size={TitleSizes["lg"]}>
+                Processed messages
+              </Title>
+            </CardTitle>
+            {/** split, grid, --desc list */}
+            <CardBody className="sent-spacing">
+              <DescriptionList
+                columnModifier={{ default: "3Col" }}
+                isInlineGrid
+                horizontalTermWidthModifier={{
+                  default: "35ch",
+                }}
+              >
+                <DescriptionListGroup>
+                  <DescriptionListDescription>
+                    <CheckIcon className="pf-u-icon-color-green" />
+                    1600 sent
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                  <DescriptionListDescription>
+                    <ExclamationIcon className="pf-u-icon-color-red" />
+                    12 not sent
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+              </DescriptionList>
+            </CardBody>
+            <CardFooter className="processed-messages-info">
+              These numbers reflect the messages processed in the last 24 hours
+            </CardFooter>
+          </Card>
+        </FlexItem>
+        <FlexItem>
+          <TextContent>
+            <TextList component={TextListVariants.dl}>
+              <TextListItem component={TextListItemVariants.dt}>
+                Error handling method
+              </TextListItem>
+              <TextListItem component={TextListItemVariants.dd}>
+                Dead letter queue
+              </TextListItem>
+              <TextListItem component={TextListItemVariants.dt}>
+                Dead letter queue topic
+              </TextListItem>
+              <TextListItem
+                className="pf-u-link-color"
+                component={TextListItemVariants.dd}
+              >
+                my-topic-2
+              </TextListItem>
+            </TextList>
+          </TextContent>
+        </FlexItem>
+        <FlexItem>
+          <Divider />
+        </FlexItem>
+      </Flex>
+    </React.Fragment>
   );
 };
