d701767dcf0d9a8c59a27d81a4b45e2a7c718b82
suyash-naithani <suyash.naithani@gmail.com>
2023-03-31 16:07:33 +0530

    Rename id

--- src/shared/Table/TableView.stories.tsx
+++ src/shared/Table/TableView.stories.tsx
@@ -102,8 +102,8 @@ Example.args = {
 export const KebabAndButtonToolbarVisible = Template.bind({});
 KebabAndButtonToolbarVisible.args = {
   data: sampleData,
-  toolbarKebabOuiaId: "kebab-dropdown-toolbar",
-  toolbarOuiaId: "table-toolbar",
+  toolbarKebabOuiaId: "instance-kebab-toolbar",
+  toolbarOuiaId: "instance-table-toolbar",
   kebabActions: [
     {
       label: "First action",
