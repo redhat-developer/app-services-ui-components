779274f8268cbc48ebf434100f5d4f8a247bd358
hemahg <hhg@redhat.com>
2023-05-03 14:21:45 +0530

    Update overview page
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/AppServices/OverviewPageV4.stories.tsx
+++ src/AppServices/OverviewPageV4.stories.tsx
@@ -3,12 +3,6 @@ import { OverviewPageV4 as OverviewPageV4Comp } from "./OverviewPageV4";
 
 export default {
   component: OverviewPageV4Comp,
-  args: {
-    toKafkaHref: "/to-kafka",
-    toServiceRegistryHref: "/to-service",
-    toConnectorsHref: "/to-connectors",
-    toAPIDesignHref: "/to-apidesigner",
-  },
 } as ComponentMeta<typeof OverviewPageV4Comp>;
 
 const Template: ComponentStory<typeof OverviewPageV4Comp> = (args) => (
