dc3886c0a56c86a7796860aee9544dc1bd734d04
Hema HG <53568062+hemahg@users.noreply.github.com>
2023-05-03 15:44:04 +0530

    chore: update overview page (#631)
    * Update overview page
    
    Signed-off-by: hemahg <hhg@redhat.com>
    
    * Update test
    
    Signed-off-by: hemahg <hhg@redhat.com>
    
    ---------
    
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/AppServices/OverviewPageV4.tsx
+++ src/AppServices/OverviewPageV4.tsx
@@ -15,36 +15,14 @@ import {
 import { ExternalLinkAltIcon } from "@patternfly/react-icons";
 import type { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
 import {
   AppServicesOverviewIconPattern,
   LogoRedHatOpenShiftApiManagementAStandardRgb,
-  LogoRedHatOpenShiftConnectorsBStandardRgb,
-  LogoRedHatOpenShiftDatabaseAccessAStandardRgb,
   LogoRedHatOpenShiftDataScienceAStandardRgb,
-  LogoRedHatOpenShiftServiceRegistryAStandardRgb,
-  LogoRedHatOpenShiftStreamsForApacheKafkaAStandardRgbPng,
-  LogoRedHatOpenShiftAPIDesignerSvg,
 } from "../images";
-import {
-  MarketingPageBanner,
-  MarketingPageHero,
-  MarketingPageSection,
-} from "./components";
+import { MarketingPageHero, MarketingPageSection } from "./components";
 
-export type OverviewPageV4Props = {
-  toKafkaHref: string;
-  toServiceRegistryHref: string;
-  toConnectorsHref: string;
-  toAPIDesignHref: string;
-};
-
-export const OverviewPageV4: FunctionComponent<OverviewPageV4Props> = ({
-  toKafkaHref,
-  toServiceRegistryHref,
-  toConnectorsHref,
-  toAPIDesignHref,
-}) => {
+export const OverviewPageV4: FunctionComponent = () => {
   const { t } = useTranslation();
   return (
     <>
@@ -52,71 +30,12 @@ export const OverviewPageV4: FunctionComponent<OverviewPageV4Props> = ({
         title={t("overview-v4:heroTitle")}
         tagLine={t("overview-v4:heroTagline")}
         description={t("overview-v4:heroDescription")}
-        description2={t("overview-v4:heroDescription2")}
         heroImage={AppServicesOverviewIconPattern}
         heroImageSize={678}
-        cta={
-          <Button
-            variant={ButtonVariant.primary}
-            ouiaId="button-try-kafka"
-            isLarge
-            component={(props) => (
-              <Link
-                {...props}
-                data-testid="hero-buttonTryKafka"
-                to={toKafkaHref}
-              />
-            )}
-          >
-            {t("overview-v4:heroCallToActionButton")}
-          </Button>
-        }
         variant="dark"
       />
-      <MarketingPageBanner>{t("overview-v4:banner")}</MarketingPageBanner>
       <MarketingPageSection>
         <Grid md={6} lg={3} hasGutter>
-          {/* Api Designer card */}
-          <Card>
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftAPIDesignerSvg}
-                  alt="Red Hat OpenShift API Designer"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview-v4:rhoapidesignerTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>{t("overview-v4:rhoapidesignerMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview-v4:rhoapidesignerSecondaryText")}
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHOAPIDesigner-buttonCTA"
-                variant={ButtonVariant.secondary}
-                component={(props) => (
-                  <Link
-                    {...props}
-                    data-testid="hero-buttonCreateDesign"
-                    to={toAPIDesignHref}
-                  />
-                )}
-              >
-                {t("overview-v4:rhoapidesignerCalltoActionButton")}
-              </Button>
-            </CardFooter>
-          </Card>
-
           {/* API Management card */}
           <Card>
             <CardHeader>
@@ -162,182 +81,6 @@ export const OverviewPageV4: FunctionComponent<OverviewPageV4Props> = ({
                 href="https://developers.redhat.com/products/rhoam/getting-started"
               >
                 {t("overview-v4:getStarted")}{" "}
-                <ExternalLinkAltIcon className="pf-u-ml-sm" />
-              </Button>
-            </CardFooter>
-          </Card>
-
-          {/* Kafka card */}
-          <Card ouiaId="card-rhosak">
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftStreamsForApacheKafkaAStandardRgbPng}
-                  alt="Red Hat OpenShift Streams for Apache Kafka logo"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview-v4:rhosakTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>{t("overview-v4:rhosakMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview-v4:rhosakSecondaryText")}
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHOSAK-buttonCreateKafka"
-                variant={ButtonVariant.secondary}
-                component={(props) => <Link {...props} to={toKafkaHref} />}
-                ouiaId="button-create"
-              >
-                {t("overview-v4:rhosakCallToActionButton")}
-              </Button>
-            </CardFooter>
-          </Card>
-
-          {/* Service Registry card */}
-          <Card>
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftServiceRegistryAStandardRgb}
-                  alt="Red Hat OpenShift Service Registry logo"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview-v4:rhosrTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>{t("overview-v4:rhosrMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview-v4:rhosrSecondaryText")}
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHOSR-buttonCTA"
-                variant={ButtonVariant.secondary}
-                component={(props) => (
-                  <Link
-                    {...props}
-                    data-testid="hero-buttonTryKafka"
-                    to={toServiceRegistryHref}
-                  />
-                )}
-              >
-                {t("overview-v4:rhosrCallToActionButton")}
-              </Button>
-            </CardFooter>
-          </Card>
-
-          {/* Connectors card */}
-          <Card>
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftConnectorsBStandardRgb}
-                  alt="Red Hat OpenShift Connectors logo"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview-v4:rhocTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>{t("overview-v4:rhocMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview-v4:rhocSecondaryText")}{" "}
-                  <Button
-                    data-testid="cardRHOC-linkOpenShift"
-                    isInline
-                    variant={ButtonVariant.link}
-                    component="a"
-                    target="_blank"
-                    href="https://openshift.com"
-                  >
-                    OpenShift
-                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
-                  </Button>
-                  {t("overview-v4:rhocSecondaryTextContinued")}
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHOC-buttonCTA"
-                variant={ButtonVariant.secondary}
-                target="_blank"
-                component={(props) => <Link {...props} to={toConnectorsHref} />}
-              >
-                {t("overview-v4:getStarted")}
-              </Button>
-            </CardFooter>
-          </Card>
-
-          {/* Database access card */}
-          <Card>
-            <CardHeader>
-              <CardHeaderMain>
-                <img
-                  src={LogoRedHatOpenShiftDatabaseAccessAStandardRgb}
-                  alt="Red Hat OpenShift Data Science logo"
-                  style={{ height: "50px" }}
-                />
-              </CardHeaderMain>
-            </CardHeader>
-            <CardTitle>
-              <Title headingLevel="h2" size="xl">
-                {t("overview-v4:dbaasTitle")}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              <Stack hasGutter>
-                <StackItem>{t("overview-v4:dbaasMainText")}</StackItem>
-                <StackItem className="pf-u-color-200">
-                  {t("overview-v4:dbaasSecondaryText")}{" "}
-                  <Button
-                    data-testid="cardRHODS-linkOpenShift"
-                    isInline
-                    variant={ButtonVariant.link}
-                    component="a"
-                    target="_blank"
-                    href="https://openshift.com"
-                  >
-                    {" "}
-                    OpenShift
-                    <ExternalLinkAltIcon className="pf-u-ml-xs" />
-                  </Button>
-                  .
-                </StackItem>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <Button
-                data-testid="cardRHODS-buttonCTA"
-                variant={ButtonVariant.secondary}
-                component="a"
-                target="_blank"
-                href="https://console.redhat.com/application-services/databases"
-              >
-                {t("overview-v4:learnMore")}{" "}
                 <ExternalLinkAltIcon className="pf-u-ml-sm" />
               </Button>
             </CardFooter>
